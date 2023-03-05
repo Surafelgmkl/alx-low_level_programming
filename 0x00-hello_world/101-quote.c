d3831ecc30b9bd297923c26ae0670c50fb94ab4d
Surafelgmkl <122892632+Surafelgmkl@users.noreply.github.com>
2023-03-05 21:01:45 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  *  main -A c program that prints a line to standard error
- *  Retern 1 (Success)
+ *  Return 1 (Success)
  */
 int main(void)
 {       
-        fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", standout);
+        fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
         return (1);
 }
